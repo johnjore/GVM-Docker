b49ca43356ef2cea52dbd40ce59ef3593f1772fe
Austin Songer <austinvernsonger@protonmail.com>
2020-06-24 23:12:24 +0000

    Update

--- components/build-dev.sh
+++ components/build-dev.sh
@@ -10,26 +10,26 @@ STAGE="dev"
 # build
 for app in gvmlibs openvas gsa ; do
   docker build -f ./Dockerfile-${app} --build-arg STAGE=${STAGE} \
-    -t "dgiorgio/${app}:dev-${DATE}${BUILD}" .
+    -t "securecompliancesolutions/${app}:dev-${DATE}${BUILD}" .
 done
 
 # build gvmd
 [ -z "${DATABASE}" ] && DATABASE="sqlite"
 [ "${DATABASE}" == "sqlite" ] && DATABASE_TAG="" || DATABASE_TAG="-${DATABASE}"
 docker build -f ./Dockerfile-gvmd --build-arg STAGE=${STAGE} --build-arg DATABASE=${DATABASE} \
-  -t "dgiorgio/gvmd:dev-${DATE}${DATABASE_TAG}${BUILD}" .
+  -t "securecompliancesolutions/gvmd:dev-${DATE}${DATABASE_TAG}${BUILD}" .
 
 # push
 if [ "${1}" == "push" ]; then
   for app in gvmlibs openvas gsa ; do
     for tag in "dev-${DATE}${BUILD}"; do
-      docker push "dgiorgio/${app}:${tag}"
+      docker push "securecompliancesolutions/${app}:${tag}"
     done
   done
 
   # push gvmd
   for tag in "dev-${DATE}${DATABASE_TAG}${BUILD}"; do
-    docker push "dgiorgio/gvmd:${tag}"
+    docker push "securecompliancesolutions/gvmd:${tag}"
   done
 fi
 
--- components/build-stable.sh
+++ components/build-stable.sh
@@ -4,7 +4,7 @@ set -e
 
 # default - vars
 STAGE="stable"
-[ -z "${BUILD}" ] && BUILD=""
+[ -z "${BUILD}" ] && BUILD="-20200613"
 
 # build gvmlibs
 echo "
@@ -13,8 +13,9 @@ echo "
 ################################################################################"
 gvmlibs_version=11.0
 build_gvmlibs="${BUILD}"
-docker build -f /components/gvm-libs/Dockerfile --build-arg STAGE=${STAGE} 
-
+docker build -f ./Dockerfile-gvmlibs --build-arg STAGE=${STAGE} \
+  -t "securecompliancesolutions/gvmlibs:${gvmlibs_version}${build_gvmlibs}" \
+  -t "securecompliancesolutions/gvmlibs:latest" .
 
 # build gvmd
 echo "
@@ -23,7 +24,9 @@ echo "
 ################################################################################"
 gvmd_version=9.0
 build_gvmd="${BUILD}"
-docker build -f /components/gvmd/Dockerfile --build-arg STAGE=${STAGE}
+docker build -f ./Dockerfile-gvmd --build-arg STAGE=${STAGE} \
+  -t "securecompliancesolutions/gvmd:${gvmd_version}${build_gvmd}" \
+  -t "securecompliancesolutions/gvmd:latest" .
 
 # build openvas
 echo "
@@ -32,8 +35,9 @@ echo "
 ################################################################################"
 openvas_version=7.0
 build_openvas="${BUILD}"
-docker build -f /components/openvas/Dockerfile --build-arg STAGE=${STAGE} 
-
+docker build -f ./Dockerfile-openvas --build-arg STAGE=${STAGE} \
+  -t "securecompliancesolutions/openvas:${openvas_version}${build_openvas}" \
+  -t "securecompliancesolutions/openvas:latest" .
 
 # build gsa
 echo "
@@ -42,19 +46,44 @@ echo "
 ################################################################################"
 gsa_version=9.0
 build_gsa="${BUILD}"
-docker build -f /components/gsa/Dockerfile --build-arg STAGE=${STAGE} 
-
+docker build -f ./Dockerfile-gsa --build-arg STAGE=${STAGE} \
+  -t "securecompliancesolutions/gsa:${gsa_version}${build_gsa}" \
+  -t "securecompliancesolutions/gsa:latest" .
 
 # build postgres
-
 echo "
 ################################################################################
 ################### Build postgres #############################################
 ################################################################################"
 postgres_version=9.6
 build_postgres="${BUILD}"
-docker build -f /components/postgres/Dockerfile --build-arg STAGE=${STAGE} 
+docker build -f ./Dockerfile-postgres --build-arg STAGE=${STAGE} \
+  -t "securecompliancesolutions/postgres:${postgres_version}${build_postgres}" \
+  -t "securecompliancesolutions/postgres:latest" .
 
 postgres_gvm_version=9.6
 build_postgres_gvm="${BUILD}"
-docker build -f /components/postgres-gvm/Dockerfile --build-arg STAGE=${STAGE} 
+docker build -f ./Dockerfile-postgres-gvm --build-arg STAGE=${STAGE} \
+  -t "securecompliancesolutions/postgres-gvm:${postgres_gvm_version}${build_postgres_gvm}" \
+  -t "securecompliancesolutions/postgres-gvm:latest" .
+
+# push
+if [ "${1}" == "push" ]; then
+  docker push "securecompliancesolutions/gvmlibs:${gvmlibs_version}${build_gvmlibs}"
+  docker push "securecompliancesolutions/gvmlibs:latest"
+
+  docker push "securecompliancesolutions/gvmd:${gvmd_version}${build_gvmd}"
+  docker push "securecompliancesolutions/gvmd:latest"
+
+  docker push "securecompliancesolutions/openvas:${openvas_version}${build_openvas}"
+  docker push "securecompliancesolutions/openvas:latest"
+
+  docker push "securecompliancesolutions/gsa:${gsa_version}${build_gsa}"
+  docker push "securecompliancesolutions/gsa:latest"
+
+  docker push "securecompliancesolutions/postgres:${postgres_version}${build_postgres}"
+  docker push "securecompliancesolutions/postgres:latest"
+
+  docker push "securecompliancesolutions/postgres-gvm:${postgres_gvm_version}${build_postgres_gvm}"
+  docker push "securecompliancesolutions/postgres-gvm:latest"
+fi
